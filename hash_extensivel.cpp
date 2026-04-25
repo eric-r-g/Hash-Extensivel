1ea5695d5cd204960d018b5a2f3bd485e4bf33d4
Paulo <p.eduardocm3@gmail.com>
2026-04-25 15:15:23 -0300

    Incluindo função de busca, calcular hash e inicializar diretorio

---
+++ hash_extensivel.cpp
@@ -0,0 +1,70 @@
+#include "hash_extensivel.h"
+
+Hash_Extensivel::Hash_Extensivel(){
+    dir.prof_global = 0;
+}
+
+void Hash_Extensivel::inicializar_diretorio(int PG){
+    dir.prof_global = PG;
+    int num_buckets = pow(2, PG);
+    dir.ponteiros.resize(num_buckets);
+
+    //criar os arquivos dos buckets
+    for(int i = 0 ; i < num_buckets ; i++){
+        string nome = "bucket_" + to_string(i+1) + ".txt";
+        dir.ponteiros[i] = nome;
+
+        ofstream arquivo(nome);
+
+        /* A estrutura eh:
+        1 linha: profundidade local (inicialmente igual a profundidade global)
+        2 linha: quatidade de chaves (inicialmente 0)
+        3 linha: 5 chaves separadas por espaco (inicialmente -1)
+        */
+        if(arquivo.is_open()){
+            arquivo << PG << "\n";
+            arquivo << 0 << "\n";
+            for(int i = 0 ; i < 5 ; i++)
+                arquivo << -1 << " ";
+
+            arquivo.close();
+        }
+        else cerr << "Erro ao abrir arquivo: " << nome << endl;
+    }
+}
+
+int Hash_Extensivel::calcular_hash(int chave){
+    //deve retornar os PG bits menos significativos
+    //equivalente a fazer o resto por 2^PG
+    int div = pow(2, dir.prof_global);
+    return chave % div;
+}
+
+void Hash_Extensivel::buscar(int chave, ofstream& arquivo_saida){
+    //encontrar e abrir o arquivo do bucket
+    int idx = calcular_hash(chave);
+    string nome = dir.ponteiros[idx];
+
+    ifstream arquivo(nome);
+
+    int qtde = 0; //quantidade de tuplas selecionadas (teoricamente eh pra dar so 1 ou 0)
+    if(arquivo.is_open()){
+        //instancia o bucket e le os dados do txt
+        bucket b; 
+        arquivo >> b.prof_local;
+        arquivo >> b.qtd_chaves;
+        for(int i = 0; i < 5; i++) {
+            arquivo >> b.chaves[i];
+        }
+
+        //procura a chave no array do bucket
+        for(int i = 0 ; i < b.qtd_chaves ; i++)
+            if(b.chaves[i] == chave) qtde++;
+
+        arquivo.close();
+    }
+    else cerr << "Erro ao abrir arquivo" << nome << endl;
+
+    //escrever no out.txt (BUS:x/<quantidade de tuplas selecionadas>)
+    arquivo_saida << "BUS:" << chave << "/<" << qtde << ">\n";
+}
--- hash_extensivel.h
+++ hash_extensivel.h
@@ -5,12 +5,13 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
 struct bucket{
     int prof_local;
-    int chaves[5]; //array com as entradas
+    int chaves[5];
     int qtd_chaves;
 };
 
@@ -23,12 +24,12 @@ class Hash_Extensivel{
     public:
         Hash_Extensivel();
 
-        int calcular_hash(int chave);
-        void inicializar_diretorio(int pg_inicial);
+        int calcular_hash(int chave); //retornar os PG bits menos significativos
+        void inicializar_diretorio(int pg_inicial); //recebe a PG e cria os buckets (.txt)
 
-        void buscar(int chave, ofstream& arquivoSaida);
-        void inserir(int chave, ofstream& arquivoSaida);
-        void remover(int chave, ofstream& arquivoSaida);
+        void buscar(int chave, ofstream& arquivo_saida);
+        void inserir(int chave, ofstream& arquivo_saida);
+        void remover(int chave, ofstream& arquivo_saida);
     
     private:
         diretorio dir;
